2a1209265febeaf30d6884e7e7dc2fad584536ad
kirtiverma17 <110229852+kirtiverma17@users.noreply.github.com>
2023-02-13 21:51:38 +0530

    Update PhotoFrame.js

--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import "..styles/App.css"
 
-export const PhotoFrame = () => {
+export const PhotoFrame = ({url,title}) => {
+   return(
+      <div className='photoframes'>
+      <div>{title}</div>
+      <img src={url}/>
+  )
    
 }
