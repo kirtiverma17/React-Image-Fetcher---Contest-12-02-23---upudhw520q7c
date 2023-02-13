82db6631c41385cd753ac5c9ba01a997d1478736
kirtiverma17 <110229852+kirtiverma17@users.noreply.github.com>
2023-02-13 20:40:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
-const App = () => {
-  
-}
 
+const fetchPosts = async (id) => {
+  let url = `https://jsonplaceholder.typicode.com/photo/${id}`;
+  return fetch(url);
+}
+const App = () => {
+  const[data,setData]=useState([])
+  const[id,setId]=useState(null)
+  const handleChange=(e)=>{
+    setId(e.target.value)
+ }
+  
+  const loadData = async () => {
+    fetchPosts(id)
+    .then((res) => res.json())
+    .then((jsonData) => {
+      setData(jsonData);
+    })
+  }
+  
+  useEffect(() => {
+    loadData();
+  },[]);
+  
+  useEffect(() => {
+    setData(null)
+    loadData()
+  }, [id])
+  
+  return(
+    <div>
+    <span>Id number</span>
+    <input type="number" onChange={handleChange} />
+    {data == null ? ( <loader/> ) : <photoFrame url={data.url} title={data.title}/>
+   </div>
+ )
+}
 
 export default App;
