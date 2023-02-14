4a9c1b16f27461ee125626aef77c7531808a04d1
kirtiverma17 <110229852+kirtiverma17@users.noreply.github.com>
2023-02-14 20:06:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect,useState } from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
-
-const fetchPosts = async (id) => {
-  let url = `https://jsonplaceholder.typicode.com/photos/${id}`;
-  return fetch(url);
+const fetchPosts = async(id) => {
+    let url =`https://jsonplaceholder.typicode.com/photos/${id}`;
+    return fetch(url);
 }
 const App = () => {
   const[data,setData]=useState([])
   const[id,setId]=useState(null)
   const handleChange=(e)=>{
     setId(e.target.value)
- }
-  
+  }
   const loadData = async () => {
     fetchPosts(id)
     .then((res) => res.json())
     .then((jsonData) => {
-      setData(jsonData);
+        setData(jsonData);
     })
   }
-  
-  useEffect(() => {
+  useEffect(() =>{
     loadData();
-  }, []);
-  
+  },[]);
   useEffect(() => {
     setData(null)
     loadData()
-  }, [id])
-  
+  },[id])
   return(
     <div>
-      <span>Id number</span>
-      <input type="number" onChange={handleChange} />
-      {data == null ? ( <Loader/> ) : <PhotoFrame url={data.url} title={data.title}/>
-   </div>
- )
+        <span>Id number</span>
+        <input type="number" onChange={handleChange}/>
+        {data == null?(<Loader/>):<PhotoFrame url={data.url} title={data.title}/>}
+    </div>
+  )
 }
+
 
 export default App;
